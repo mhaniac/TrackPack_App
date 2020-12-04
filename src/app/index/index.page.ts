60f5a274fed9a4c819ff4e285c7aa80a35dd7e22
Angel Castillo <portillocastilloa@gmail.com>
2020-12-03 22:37:27 -0600

    Codigo QR funcionando

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { UserService } from '../services/user.service';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-index',
@@ -9,10 +10,18 @@ import { UserService } from '../services/user.service';
 })
 export class IndexPage implements OnInit {
 
-  constructor(private loginService: LoginService, public userService: UserService) { }
+  constructor(private loginService: LoginService, public userService: UserService,private barcodeScanner: BarcodeScanner) { }
 
   ngOnInit() {
 
+  }
+
+  startScanner(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
   }
 
 }
